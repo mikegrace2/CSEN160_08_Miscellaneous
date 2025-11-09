5e75b35acdbcc1e5595afc88a6e2fa730113e7b4
Michael Schimpf <mschimpf@scu.edu>
2025-11-09 05:38:28 -0500

    Updates

---
+++ src/main/java/com/csen160/I_Log4J/MainExecutor.java
@@ -0,0 +1,20 @@
+package com.csen160.I_Log4J;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MainExecutor {
+    private static final Logger logger = LogManager.getLogger(MainExecutor.class);
+
+    // example replacement of System.out.println
+    public static double calculate_Mult() {
+        long start = System.currentTimeMillis();
+        double returnVal = 0;
+        for (long i = 0; i < 500_000_000; i++) {
+            returnVal = returnVal * i * (Math.sqrt(3456) * 3343454.7f);
+        }
+        long end = System.currentTimeMillis();
+        logger.info("{} Multiplications: Time in ms: {}", Thread.currentThread().getName(), (end - start));
+        return returnVal;
+    }
+}
