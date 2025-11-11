006491737b69524106bc11228433fbf3f8610abc
Michael Schimpf <mschimpf@scu.edu>
2025-11-11 11:37:05 -0500

    Made it a bit more convenient

--- src/main/java/com/csen160/G_Optimizations/B_Fibonacci.java
+++ src/main/java/com/csen160/G_Optimizations/B_Fibonacci.java
@@ -2,6 +2,7 @@ package com.csen160.G_Optimizations;
 
 
 public class B_Fibonacci {
+    // Recursive Fibonacci
     public static long fib(int n) {
         if (n <= 2) {
             return 1;
@@ -9,7 +10,8 @@ public class B_Fibonacci {
             return fib(n - 2) + fib(n - 1);
         }
     }
-    
+
+    // For-Loop Fibonacci
     public static int fib2(int n){
       int f[] = new int[n+1];
 
@@ -23,19 +25,23 @@ public class B_Fibonacci {
      }
     
     public static void main(String[] args) {
+        // Change this value to compute more or fewer Fibonacci numbers
+        int howManyTimes = 46; // 46 is very slow for recursive
+
     	long start1=System.currentTimeMillis();
-        for (int i = 1; i <= 45; i++) {
+        for (int i = 1; i <= howManyTimes; i++) {
             System.out.println(i+". "+fib(i));
         }
         long end1=System.currentTimeMillis();
         
     	long start2=System.currentTimeMillis();
-        for (int i = 1; i <= 45; i++) {
+        for (int i = 1; i <= howManyTimes; i++) {
             System.out.println(i+". "+fib2(i));
         }
         long end2=System.currentTimeMillis();
 
         System.out.println();
+        System.out.println("howManyTimes = "+howManyTimes);
         System.out.println("Fibonacci Recursive Time in ms: "+(end1-start1));
         System.out.println("Fibonacci For-Loop  Time in ms: "+(end2-start2));
     }
