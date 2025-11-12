74624972278b8707d3d25de460ed5c003cc476e0
Michael Schimpf <mschimpf@scu.edu>
2025-11-12 12:18:18 -0500

    Updated size a little bit

--- src/main/java/com/csen160/G_Optimizations/D_IsPrimeNumber.java
+++ src/main/java/com/csen160/G_Optimizations/D_IsPrimeNumber.java
@@ -24,7 +24,7 @@ public class D_IsPrimeNumber {
 	}
 
 	public static void main(String[] args) {
-		int SIZE=100_000_000;
+		int SIZE=300_000_000;
 		int[] numberArr=new int[SIZE];
 		for (int i=0;i<SIZE;i++) {
 			numberArr[i]=(int)(Math.random() * 100);
@@ -35,13 +35,13 @@ public class D_IsPrimeNumber {
 			D_IsPrimeNumber.isPrime(numberArr[i]);
 		}
 		long end1=System.currentTimeMillis();
-		System.out.println("isPrime standard="+(end1-start1));
+		System.out.println("SIZE="+SIZE+" isPrime standard="+(end1-start1));
 		
 		long start2=System.currentTimeMillis();
 		for (int i=0;i<SIZE;i++) {
 			D_IsPrimeNumber.isPrime2(numberArr[i]);
 		}
 		long end2=System.currentTimeMillis();
-		System.out.println("isPrime dynamic="+(end2-start2));
+		System.out.println("SIZE="+SIZE+" isPrime dynamic="+(end2-start2));
 	}
 }
